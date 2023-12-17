7c9d2f2dbdf4aa7cfb157d5c2e83346227c40354
Jack Spence <jackspence123456@gmail.com>
2023-12-17 10:35:36 +0200

    Added input, Queries and mutations to typeDefs.

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -23,6 +23,26 @@ const typeDefs = gql`
 		token: ID!
 		user: User
 	}
+
+	input BookInput {
+		authors: [String]
+		description: String!
+		bookId: String!
+		image: String
+		link: String
+		title: String!
+	}
+
+	type Query {
+		me: User
+	}
+
+	type Mutation {
+		login(email: String!, password: String!): Auth
+		addUser(username: String!, email: String!, password: String!): Auth
+		saveBook(bookData: BookInput!): User
+		removeBook(bookId: ID!): User
+	}
 `;
 
 // Exports
