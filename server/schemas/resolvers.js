52d81978b55342ab0b466a1a00576b68069bdc69
Jack Spence <jackspence123456@gmail.com>
2023-12-17 11:14:22 +0200

    Added mutations and queries to resolvers.js

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,5 +1,7 @@
 // Imports
 const { User } = require("../models");
+const { AuthenticationError } = require("apollo-server-express");
+const { signToken } = require("../utils/auth");
 
 const resolvers = {
 	Query: {
@@ -10,6 +12,62 @@ const resolvers = {
 				);
 				return userData;
 			}
+			throw new AuthenticationError("Please Login first!");
+		},
+	},
+
+	Mutation: {
+		login: async (parent, { email, password }) => {
+			const user = await User.findOne({ email });
+
+			if (!user) {
+				throw new AuthenticationError(
+					"Please enter the correct email or password for your account!"
+				);
+			}
+
+			const userPassword = await User.isCorrectPassword(password);
+
+			if (!userPassword) {
+				throw new AuthenticationError(
+					"Please enter the correct email or password for your account!"
+				);
+			}
+
+			const token = signToken(user);
+			return { token, user };
+		},
+		addUser: async (parent, args) => {
+			const user = await User.create(args);
+			const token = signToken(user);
+
+			return { token, user };
+		},
+		saveBook: async (parent, { bookData }, context) => {
+			if (context.user) {
+				const updatedUser = await User.findByIdAndUpdate(
+					{ _id: context.user._id },
+					{ $push: { savedBooks: bookData } },
+					{ new: true }
+				);
+
+				return updatedUser;
+			}
+
+			throw new AuthenticationError("Please Login first!");
+		},
+		removeBook: async (parent, { bookId }, context) => {
+			if (context.user) {
+				const updatedUser = await User.findOneAndUpdate(
+					{ _id: context.user._id },
+					{ $pull: { savedBooks: { bookId } } },
+					{ new: true }
+				);
+
+				return updatedUser;
+			}
+
+			throw new AuthenticationError("Please Login first!");
 		},
 	},
 };
